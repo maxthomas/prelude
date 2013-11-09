3b176a1573b6ec967c51decd363b336f4e6ff91c
Max Thomas <maxjthomas@gmail.com>
2013-11-09 16:54:31 -0500

    add personal init file

---
+++ personal/init.el
@@ -0,0 +1,30 @@
+;;; Initialize --- Provides local customizations
+
+;;; Commentary:
+
+;;; Code:
+
+(disable-theme 'zenburn)
+
+(require 'package)
+(add-to-list 'package-archives
+            '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(twilight-theme
+                      hlinum
+                      paredit)
+  "A list of packages to ensure are installed at launch.")
+
+;; Automaticaly install any missing packages
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-hook 'cider-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'paredit-mode)
